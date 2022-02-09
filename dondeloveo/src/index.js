d9306c81fa757349d5d1cf44349b3acf1ec5cac4
hgavela <hugogavelagarcia@gmail.ocom>
2022-02-09 22:40:02 +0100

    menu bug fixed

--- dondeloveo/src/index.js
+++ dondeloveo/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './/css/style.css';
 import reportWebVitals from './reportWebVitals';
 import Menu from './header';
 
