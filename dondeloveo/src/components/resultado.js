aa5a10a519bb17db8d82da25f2c61cb73c802dfc
hgavela <hugogavelagarcia@gmail.ocom>
2022-02-18 21:58:39 +0100

    api added

--- dondeloveo/src/components/resultado.js
+++ dondeloveo/src/components/resultado.js
@@ -3,60 +3,16 @@ import "../css/header.css";
 import "../css/resultado.css";
 import netflix from "../img/netflix.svg";
 import banner from "../img/banner.svg";
-import { useParams } from "react-router-dom";
 
 
-let name, picture;
+function Resultado (props) {
+    console.log(props);
 
-var axios = require("axios").default;
-
-
-async function getData(id) {
-    var options = {
-        method: "GET",
-        url: "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup",
-        params: { source_id: id, source: "imdb", country: "es" },
-        headers: {
-          "x-rapidapi-host":
-            "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
-          "x-rapidapi-key": "271b1c27e1msh55702f2ddad1a71p10ec12jsn8bd2cf85a04e",
-        },
-      };
-  try {
-    const response = await axios(options);
-    name = response.data.collection.name;
-    picture = response.data.collection.picture;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-
-function Resultado(props) {
-    const id = useParams().id;
- getData(id);
-
-  /*
-  axios
-    .request(options)
-    .then(function (response) {
-      console.log(response.data);
-      name = response.data.collection.name;
-      picture = response.data.collection.picture;
-      console.log(response.data.collection.locations[0].display_name);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });*/
-
-    // esperar a que termine la funcion get data
-
-
-  return(
+  return (
     <section className="resultado">
-      <h1>{name}</h1>
+      <h1>{props.collection.name}</h1>
       <div className="banner">
-        <img src={picture} alt={name} />
+        <img src={props.collection.picture} alt={props.collection.name} />
       </div>
       <h1>Plataformas</h1>
       <div className="plataformas">
--- dondeloveo/src/pages/Result.js
+++ dondeloveo/src/pages/Result.js
@@ -2,16 +2,104 @@ import React from "react";
 import Menu from "../components/header";
 import Footer from "../components/footer";
 import Resultado from "../components/resultado";
+import { useParams } from "react-router-dom";
 
-const Result = () => {
-    return(
-        <div className="contenido">
-            <Menu />
-            <Resultado />
-            <Footer />
-        </div>
-    )
 
+
+class Result extends React.Component {
+  
+  constructor() {
+    super();
+    var resultado = this.getData();
+    this.state = { data: resultado };
+    
+  }
+  
+  async componentWillMount() {
+      /*
+    console.log("hola");
+    const response = await fetch("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=tt2661044&source=imdb&country=es", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+            "x-rapidapi-key": "271b1c27e1msh55702f2ddad1a71p10ec12jsn8bd2cf85a04e"
+        }
+    });
+    const json = await response.json();
+    this.setState({ data: json });
+    */
+}
+
+
+
+  /*
+  data = async (id) => {
+    var options = {
+      method: "GET",
+      url: "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup",
+      params: { source_id: id, source: "imdb", country: "es" },
+      headers: {
+        "x-rapidapi-host":
+          "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+        "x-rapidapi-key": "271b1c27e1msh55702f2ddad1a71p10ec12jsn8bd2cf85a04e",
+      },
+    };
+    try {
+      const response = await axios(options);
+      console.log(response.data);
+      this.setState({ data: response.data });
+    } catch (error) {
+      console.log(error);
     }
+  };*/
+  /*
+  componentDidMount() {
+    console.log("hola");
+    var options = {
+      method: "GET",
+      url: "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup",
+      params: { source_id: "tt2661044", source: "imdb", country: "es" },
+      headers: {
+        "x-rapidapi-host":
+          "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+        "x-rapidapi-key": "271b1c27e1msh55702f2ddad1a71p10ec12jsn8bd2cf85a04e",
+      },
+    };
+    try {
+      const response = axios(options);
+      console.log(response.data);
+      this.setState({ data: response.data });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  */
+
+   getData = async () => {
+    console.log("hola");
+    const response = await fetch("https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/idlookup?source_id=tt2661044&source=imdb&country=es", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+            "x-rapidapi-key": "271b1c27e1msh55702f2ddad1a71p10ec12jsn8bd2cf85a04e"
+        }
+    });
+    const json = await response.json();
+    console.log(json);
+    return json;
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Menu />
+        <Resultado props={this.state.data} key={this.state.data.id} />
+        <Footer />
+      </div>
+    );
+  }
+}
+
 
 export default Result;
